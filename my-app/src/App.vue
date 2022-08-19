2eec6f5857df43a352b46d5aaf607cdf01709a6f
paras231 <sharmaparas553@gmail.com>
2022-08-19 22:02:44 +0530

    learned vue

---
+++ my-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ToDoItem label="My ToDo Item" :done="true" />
+    <ToDoItem label="My ToDo Item" :done="false" />
+    <ToDoItem label="My ToDo Item" :done="true" />
+    <ToDoItem label="New Todo Item " :done="false" />
+    <MyPractice />
+  </div>
+</template>
+
+<script>
+
+import ToDoItem from './components/ToDoItem.vue';
+import MyPractice from './components/MyPractice.vue';
+export default {
+  name: 'App',
+  components: {
+    ToDoItem,
+    MyPractice
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-app/src/components/MyPractice.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+ <h1>HELLO WORLD THIS IS PRACTICE</h1>
+    </div>
+</template>
+
+<script >
+
+export default {}
+
+</script>
+
