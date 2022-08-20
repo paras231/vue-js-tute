a7177a60837d3e974d180ee03c7570fea13e1a66
paras231 <sharmaparas553@gmail.com>
2022-08-20 13:57:16 +0530

    updated

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,37 +1,155 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
-    <ToDoItem label="My ToDo Item" :done="true" />
-    <ToDoItem label="My ToDo Item" :done="false" />
-    <ToDoItem label="My ToDo Item" :done="true" />
-    <ToDoItem label="New Todo Item " :done="false" />
-    <MyPractice />
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <ul aria-labelledby="list-summary" class="stack-large">
+      <li v-for="item in ToDoItems" :key="item.id">
+        <ToDoItem :label="item.label" :done="item.done" :id="item.id" />
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
 
 import ToDoItem from './components/ToDoItem.vue';
-import MyPractice from './components/MyPractice.vue';
+import uniqueId from 'lodash.uniqueid';
+import ToDoForm from './components/ToDoForm.vue';
 export default {
   name: 'App',
   components: {
     ToDoItem,
-    MyPractice
+    ToDoForm
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+        { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
+        { id: uniqueId('todo-'), label: 'Have fun', done: true },
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
+      ]
+    }
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({
+        id: uniqueId('todo-'), label: toDoLabel, done: false
+      })
+    }
   }
 }
+
 </script>
 
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style >
+/* Global styles */
+.btn {
+  padding: 0.8rem 1rem 0.7rem;
+  border: 0.2rem solid #4d4d4d;
+  cursor: pointer;
+  text-transform: capitalize;
 }
-h1{
-    color:yellow;
+.btn__danger {
+  color: #fff;
+  background-color: #ca3c3c;
+  border-color: #bd2130;
+}
+.btn__filter {
+  border-color: lightgrey;
+}
+.btn__danger:focus {
+  outline-color: #c82333;
+}
+.btn__primary {
+  color: #fff;
+  background-color: #000;
+}
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+}
+.btn-group > * {
+  flex: 1 1 auto;
+}
+.btn-group > * + * {
+  margin-left: 0.8rem;
+}
+.label-wrapper {
+  margin: 0;
+  flex: 0 0 100%;
+  text-align: center;
+}
+[class*="__lg"] {
+  display: inline-block;
+  width: 100%;
+  font-size: 1.9rem;
+}
+[class*="__lg"]:not(:last-child) {
+  margin-bottom: 1rem;
+}
+@media screen and (min-width: 620px) {
+  [class*="__lg"] {
+    font-size: 2.4rem;
+  }
+}
+.visually-hidden {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap;
+}
+[class*="stack"] > * {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.stack-small > * + * {
+  margin-top: 1.25rem;
+}
+.stack-large > * + * {
+  margin-top: 2.5rem;
+}
+@media screen and (min-width: 550px) {
+  .stack-small > * + * {
+    margin-top: 1.4rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.8rem;
+  }
+}
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 2rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+}
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+#app > form {
+  max-width: 100%;
+}
+#app h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 1rem;
 }
 </style>
--- my-app/src/components/MyPractice.vue
+++ my-app/src/components/MyPractice.vue
@@ -1,19 +1,44 @@
 <template>
     <div>
- <h1>HELLO WORLD THIS IS PRACTICE</h1>
+        <h1>HELLO WORLD THIS IS PRACTICE</h1>
+        <div>
+            <p v-for="item in myData" :key="item.id">{{ item.name }}</p>
+        </div>
     </div>
 </template>
 
 <script >
 
-export default {}
+export default {
+    data() {
+        return {
+            myData: [
+                {
+                    id: 1,
+                    name: 'Vue',
+                    price: 100
+                },
+                {
+                    id: 2,
+                    name: 'Angular',
+                    price: 200
+                },
+                {
+                    id: 3,
+                    name: 'React',
+                    price: 300
+                }
+            ]
+        }
+    }
+}
 
 </script >
 
 
 // basic styling in vue js
 <style scoped>
-h1{
-    color:red;
+h1 {
+    color: red;
 }
 </style>
